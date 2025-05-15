fd56cfe2cc40043418cd2e5d07ebe628cdf8fc14
Adam Hill <adam.hill@payroc.com>
2025-05-15 17:03:43 +0100

    feat: initial test harness outline [DEVX-311]

---
+++ src/Payroc.TestHarness/Program.cs
@@ -0,0 +1,19 @@
+﻿using Payroc;
+
+Console.WriteLine("Starting Payroc SDK test harness...");
+
+var apiKey = Environment.GetEnvironmentVariable("PAYROC_API_KEY");
+
+if (string.IsNullOrEmpty(apiKey))
+{
+    Console.WriteLine("Please set the PAYROC_API_KEY environment variable.");
+    return;
+}
+
+var client = new PayrocClient(
+    apiKey,
+    new ClientOptions
+    {
+        Environment = PayrocEnvironment.UAT
+    }
+);
