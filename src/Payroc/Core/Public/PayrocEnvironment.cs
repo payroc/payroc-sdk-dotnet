126d5fd4d98241db5ce7836d93d27e3757de62f1
Adam Hill <adam.hill@payroc.com>
2025-05-16 17:19:13 +0100

    DEVX-312 Fix for urls

--- src/Payroc/Core/Public/PayrocEnvironment.cs
+++ src/Payroc/Core/Public/PayrocEnvironment.cs
@@ -5,13 +5,13 @@ public class PayrocEnvironment
     public static readonly PayrocEnvironment Production = new PayrocEnvironment
     {
         Api = "https://api.payroc.com/v1",
-        Identity = "https://identity.payroc.com/v1",
+        Identity = "https://identity.payroc.com",
     };
 
     public static readonly PayrocEnvironment Test = new PayrocEnvironment
     {
         Api = "https://api.uat.payroc.com/v1",
-        Identity = "https://identity.uat.payroc.com/v1",
+        Identity = "https://identity.uat.payroc.com",
     };
 
     /// <summary>
