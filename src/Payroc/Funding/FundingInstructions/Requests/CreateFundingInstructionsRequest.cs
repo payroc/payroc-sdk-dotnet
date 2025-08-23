cf2a3b5819dfbadd3bd4bcecfdb56e8c188ee748
Adam Hill <adam.hill@payroc.com>
2025-08-23 16:44:04 +0100

    DEVX-461 Noise elimination

--- src/Payroc/Funding/FundingInstructions/Requests/CreateFundingInstructionsRequest.cs
+++ src/Payroc/Funding/FundingInstructions/Requests/CreateFundingInstructionsRequest.cs
@@ -8,7 +8,7 @@ namespace Payroc.Funding.FundingInstructions;
 public record CreateFundingInstructionsRequest
 {
     /// <summary>
-    /// Unique identifier that you generate for each request. You must use the UUID v4 format for the identifier. For more information about the idempotency key, go to [Idempotency](https://docs.payroc.com/api/idempotency).
+    /// Unique identifier that you generate for each request. You must use the [UUID v4 format](https://www.rfc-editor.org/rfc/rfc4122) for the identifier. For more information about the idempotency key, go to [Idempotency](https://docs.payroc.com/api/idempotency).
     /// </summary>
     [JsonIgnore]
     public required string IdempotencyKey { get; set; }
